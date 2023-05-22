9b1700e8ad7965b6f4501548178ec6697688c2ca
Zuby Javed <zubyjaved@gmail.com>
2023-05-22 14:31:43 -0700

    remove leetcode problems json

--- dist/background/background.js
+++ dist/background/background.js
@@ -1,22 +1,11 @@
 import { getChatGPTAccessToken } from './chatgpt/chatgpt.js';
+
 chrome.runtime.onMessage.addListener((request) => {
     if (request.type === 'OPEN_LOGIN_PAGE') {
         chrome.tabs.create({ url: 'https://chat.openai.com' });
     }
 });
-// On extension install, store the JSON of leetcode problems in local storage
-chrome.runtime.onInstalled.addListener(() => {
-    const jsonUrl = chrome.runtime.getURL('assets/data/leetcode_solutions.json');
-    fetch(jsonUrl)
-        .then((response) => response.json())
-        .then((data) => {
-        // Store the JSON data in a global variable or a storage API
-        chrome.storage.local.set({ leetcodeProblems: data });
-    })
-        .catch((error) => {
-        console.error(error);
-    });
-});
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     const urlPattern = /^https:\/\/leetcode\.com\/problems\/.*\/solutions\/?/;
     if (changeInfo.status === 'complete' && tab.url && tab.url.match(urlPattern)) {
